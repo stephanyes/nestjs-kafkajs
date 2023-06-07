4facde6b0826d3d8e5caa3b80cd0f00c48a60d14
Stephano Iucciolino <stephanoiucciolino@gmail.com>
2023-06-07 16:24:11 -0300

    retry added when creating producers and error handling

--- src/kafka/producer/kafkajs/kafkajs.producer.ts
+++ src/kafka/producer/kafkajs/kafkajs.producer.ts
@@ -7,30 +7,49 @@ export class KafkajsProducer implements IProducer {
     private readonly kafka: Kafka;
     private readonly producer: Producer;
     private readonly logger: Logger;
+    private sleepTime: number;
 
-    constructor( private readonly topic: string, broker: string) {
+    constructor( private readonly topic: string, broker: string, sleepTime: number = 5000) {
+        const b = broker.split(",").map(b => b.trim())
         this.kafka = new Kafka({
-            brokers: [broker],
+            brokers: [...b],
             logLevel: logLevel.INFO
         })
 
         this.producer = this.kafka.producer();
         this.logger = new Logger(topic)
+        this.sleepTime = sleepTime;
     }
 
-    async produce(message: Message) {
-        await this.producer.send({ topic: this.topic, messages: [message] })
-    }
-
-    async connect() {
+    async produce(message: Message, retryCount = 0) {
         try {
-            await this.producer.connect(); // kafka js by default will retry 5 times
+            await this.producer.send({ topic: this.topic, messages: [message] }) // Aca se puede agregar un key para que los mensajes con la misma key vayan al mismo partition
         } catch (error) {
-            this.logger.error(`Failed to connect to Kafka`, error);
-            await sleep(5000); // TODO maybe get this from .env? 
-            await this.connect();
+            if (retryCount < 5) {
+                this.logger.error(`Failed attepmt to produce message to topic ${this.topic}. Retrying in ${this.sleepTime}ms...`, error)
+                await sleep(this.sleepTime)
+                await this.produce(message, retryCount + 1)
+            } else {
+                this.logger.error(`Failed to produce message to topic ${this.topic} after ${retryCount} attempts. Giving up.`, error)
+                throw error;
+            }
         }
     }
+
+    async connect(retryCount = 0) {
+        try {
+          await this.producer.connect(); // kafka js by default will retry 5 times
+        } catch (error) {
+          if (retryCount < 5) {
+            this.logger.error(`Failed to connect to Kafka. Retrying in ${this.sleepTime}ms...`, error);
+            await sleep(this.sleepTime); // TODO maybe get this from .env? 
+            await this.connect(retryCount + 1);
+          } else {
+            this.logger.error(`Failed to connect to Kafka after ${retryCount} attempts. Giving up.`, error);
+            throw error; // You can choose to throw the error or handle it differently based on your requirements
+          }
+        }
+      }
 
     async disconnect() {
         await this.producer.disconnect();
--- src/kafka/producer/kafkajs/producer.service.ts
+++ src/kafka/producer/kafkajs/producer.service.ts
@@ -6,28 +6,90 @@ import { KafkajsProducer } from './kafkajs.producer';
 
 @Injectable()
 export class ProducerService implements OnApplicationShutdown {
-    private readonly producers = new Map<string, IProducer>();
+  private readonly producers = new Map<string, IProducer>();
 
-    constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
 
-    async produce(topic: string, message: Message) {
-        const producer = await this.getProducer(topic);
-        await producer.produce(message);
+  async produce(topic: string, message: Message) {
+    try {
+      const producer = await this.getProducer(topic);
+      await producer.produce(message);
+    } catch (error) {
+      // handle error here, for example log it
+      console.error(`Error producing message to topic ${topic}:`, error);
     }
+  }
 
-    async getProducer(topic: string) {
-        let producer = this.producers.get(topic);
-        if (!producer) {
-            producer = new KafkajsProducer(topic, this.configService.get<string>('KAFKA_BROKER'));
-            await producer.connect();
-            this.producers.set(topic, producer);
-        }
-        return producer;
+  async getProducer(topic: string) {
+    let producer = this.producers.get(topic);
+    if (!producer) {
+      try {
+        producer = new KafkajsProducer(topic, this.configService.get<string>('KAFKA_BROKER'));
+        await producer.connect();
+        this.producers.set(topic, producer);
+      } catch (error) {
+        // handle error here, for example log it
+        console.error(`Error creating producer for topic ${topic}:`, error);
+        throw error;
+      }
     }
+    return producer;
+  }
 
-    async onApplicationShutdown(signal?: string) {
-        for(const producer of this.producers.values()) {
-            await producer.disconnect();
-        }
+  async onApplicationShutdown(signal?: string) {
+    for (const producer of this.producers.values()) {
+      try {
+        await producer.disconnect();
+      } catch (error) {
+        // handle error here, for example log it
+        console.error(`Error disconnecting producer ${producer}:`, error);
+      }
     }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Injectable, OnModuleInit, OnApplicationShutdown } from '@nestjs/common';
+// import { Message } from 'kafkajs';
+// import { ConfigService } from '@nestjs/config';
+// import { IProducer } from './producer.interface';
+// import { KafkajsProducer } from './kafkajs.producer';
+
+// @Injectable() // we are making this service a provider here
+// export class ProducerService implements OnApplicationShutdown {
+//     private readonly producers = new Map<string, IProducer>();
+
+//     constructor(private readonly configService: ConfigService) {}
+
+//     async produce(topic: string, message: Message) {
+//         const producer = await this.getProducer(topic);
+//         await producer.produce(message);
+//     }
+
+//     async getProducer(topic: string) {
+//         let producer = this.producers.get(topic);
+//         if (!producer) {
+//             // TODO KafkaJSProducer should receive more config options like ssl, sasl, etc
+//             producer = new KafkajsProducer(topic, this.configService.get<string>('KAFKA_BROKER'));
+//             await producer.connect();
+//             this.producers.set(topic, producer);
+//         }
+//         return producer;
+//     }
+
+//     async onApplicationShutdown(signal?: string) {
+//         for(const producer of this.producers.values()) {
+//             await producer.disconnect();
+//         }
+//     }
+// }
