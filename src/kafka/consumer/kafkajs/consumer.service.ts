16036f78eaed62ceca0dcb0360efbcdd34d78e69
Stephano Iucciolino <stephanoiucciolino@gmail.com>
2023-06-07 16:26:47 -0300

    kafka consumer & consumer service modified but not working :(

--- src/kafka/consumer/kafkajs/consumer.service.ts
+++ src/kafka/consumer/kafkajs/consumer.service.ts
@@ -1,29 +1,89 @@
-import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { Inject, Injectable, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IConsumer } from './consumer.interface';
 import { IKafkajsConsumerOptions } from './kafkajs-consumer-options-interface';
-import { KafkajsConsumer } from './kaflajs.consumer';
+import { KafkajsConsumer } from './kafkajs.consumer';
+import { KAFKA_CLIENT } from 'src/kafka/constants';
 
 @Injectable()
 export class ConsumerService implements OnApplicationShutdown {
-    private readonly consumers: IConsumer[] = [];
+  private readonly consumers: IConsumer[] = [];
 
-    constructor(private readonly configService: ConfigService) {}
+  constructor(
+    @Inject(KAFKA_CLIENT) private readonly kafka,
+    private readonly configService: ConfigService,
+  ) {}
 
-    async consume({ topic, config, onMessage }: IKafkajsConsumerOptions) {
-        const consumer = new KafkajsConsumer(
-            topic, 
-            config, 
-            this.configService.get(`KAFKA_BROKER`)
-        )
-        await consumer.connect();
-        await consumer.consume(onMessage)
-        this.consumers.push(consumer);
+  async consume({ topic, config, onMessage }: IKafkajsConsumerOptions) {
+    try {
+        // console.log("KAFKA IN CONSUMERSERVICE ", this.kafka)
+        console.log("QUE kafka LLEGA ", this.kafka)
+      const consumer = new KafkajsConsumer(
+        topic,
+        config,
+        this.kafka,
+        this.configService.get('KAFKA_BROKER'),
+      );
+      await consumer.connect();
+      await consumer.consume(onMessage);
+      this.consumers.push(consumer);
+    } catch (error) {
+      // handle error here, for example log it
+      console.error(`Error consuming messages from topic ${topic.topics}:`, error);
     }
+  }
 
-    async onApplicationShutdown(signal?: string) {
-        for (const consumer of this.consumers) {
-            await consumer.disconnect();
-        }
+  async onApplicationShutdown(signal?: string) {
+    for (const consumer of this.consumers) {
+      try {
+        await consumer.disconnect();
+      } catch (error) {
+        // handle error here, for example log it
+        console.error(`Error disconnecting consumer ${consumer}:`, error);
+      }
     }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+// import { Inject, Injectable, OnApplicationShutdown } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { IConsumer } from './consumer.interface';
+// import { IKafkajsConsumerOptions } from './kafkajs-consumer-options-interface';
+// import { KafkajsConsumer } from './kafkajs.consumer';
+// import { KAFKA_CLIENT } from 'src/kafka/constants';
+
+// @Injectable()
+// export class ConsumerService implements OnApplicationShutdown {
+//     private readonly consumers: IConsumer[] = [];
+
+//     constructor(@Inject(KAFKA_CLIENT) private readonly kafka, private readonly configService: ConfigService) {}
+
+//     async consume({ topic, config, onMessage }: IKafkajsConsumerOptions) {
+//         // TODO KafkajsConsumer should receive more config options like ssl, sasl, etc
+//         const consumer = new KafkajsConsumer(
+//             topic, 
+//             config, 
+//             this.kafka,
+//             this.configService.get(`KAFKA_BROKER`)
+//         )
+//         await consumer.connect();
+//         await consumer.consume(onMessage)
+//         this.consumers.push(consumer);
+//     }
+
+//     async onApplicationShutdown(signal?: string) {
+//         for (const consumer of this.consumers) {
+//             await consumer.disconnect();
+//         }
+//     }
+// }
--- src/kafka/consumer/kafkajs/kafkajs.consumer.ts
+++ src/kafka/consumer/kafkajs/kafkajs.consumer.ts
@@ -1,22 +1,32 @@
-import { Logger } from "@nestjs/common";
+import { Logger, HttpException, HttpStatus } from "@nestjs/common";
 import { Consumer, ConsumerConfig, ConsumerSubscribeTopics, Kafka, KafkaMessage, logLevel } from "kafkajs";
 import { sleep } from "src/utils/sleep";
 import { IConsumer } from "./consumer.interface";
 import * as retry from 'async-retry';
 
 export class KafkajsConsumer implements IConsumer {
-    private readonly kafka: Kafka;
+    private readonly client: Kafka;
     private readonly consumer: Consumer;
     private readonly logger: Logger;
+    private maxRetries: number = 3;
+    private sleepTime: number;
 
     constructor(
         private readonly topic: ConsumerSubscribeTopics,
-        config: ConsumerConfig,
-        broker: string
+        consumerConfig: ConsumerConfig,
+        client: Kafka,
+        broker: string,
+        maxRetries: number = 3,
+        sleepTime: number = 5000
     ) {
-        this.kafka = new Kafka({ brokers: [broker], logLevel: logLevel.INFO });
-        this.consumer = this.kafka.consumer(config);
-        this.logger = new Logger(`${topic.topics}-${config.groupId}`);
+        // this.kafka = new Kafka({ brokers: [broker], logLevel: logLevel.INFO });
+        console.log("CONSUMER CONFIG ", consumerConfig)
+        this.client = client;
+        console.log("KAFKA ", this.client)
+        this.consumer = this.client.consumer(consumerConfig);
+        this.logger = new Logger(`${topic.topics}-${consumerConfig.groupId}`);
+        this.maxRetries = maxRetries;
+        this.sleepTime = sleepTime;
     }
 
     async connect(): Promise<void> {
@@ -24,7 +34,7 @@ export class KafkajsConsumer implements IConsumer {
             await this.consumer.connect(); // kafka js by default will retry 5 times
         } catch (error) {
             this.logger.error(`Failed to connect to Kafka`, error);
-            await sleep(5000);
+            await sleep(this.sleepTime);
             await this.connect();
         }
     }
@@ -35,18 +45,34 @@ export class KafkajsConsumer implements IConsumer {
 
     async consume(onMessage: (message: KafkaMessage) => Promise<void>): Promise<void> {
         await this.consumer.subscribe(this.topic);
+        // const {HEARTBEAT}  = this.consumer.events // instrumentation events doc -> https://kafka.js.org/docs/instrumentation-events
+        // console.log("HEATBEAT ", HEARTBEAT)
+        // todo CAMBAR EACHMESSAGE POR EACHBATCH Y VER COMO SE COMPORTA
         await this.consumer.run({
             eachMessage: async ({ topic, partition, message }) => {
                 this.logger.debug(`Processing message partition: ${partition}`);
                 this.logger.log(`Received message`, JSON.stringify({ topic, partition, message: message.value.toString() }));
                 try {
-                    await retry(async () => onMessage(message), { retries: 3, onRetry: (error, attempt) => this.logger.error(`Error consuming message, executing retry ${attempt}/3`, error) });
-                } catch (error) {
+                    // we'll try to consume the message 3 times before giving up
+                    await retry(async () => onMessage(message), { retries: this.maxRetries, onRetry: (error, attempt) => this.logger.error(`Error consuming message, executing retry ${attempt}/${this.maxRetries}`, error) });
+                } catch (error: any) { // this is not OK hay que ver como hacer para que no sea any y poder catchear el error de too many requests
+                    if (error.name === HttpStatus.TOO_MANY_REQUESTS) {
+
+                    }
                     this.logger.error(`Error consuming message`, error)
+                    this.logger.error(`Message lost: ${message.value.toString()}`)
+                    
                     // Do something with the message that we are not consuming?\
                     // add it to a dead letter queue?
                     // topic: this.topic
                     // message: message.value
+
+                    /**
+                     * Enviar a un topic de errores: Puedes redirigir los mensajes que generan errores a un topic de errores separado. 
+                     * Esto te permite separar los mensajes fallidos del flujo principal y tomar acciones específicas para manejarlos. 
+                     * Por ejemplo, puedes tener un consumidor dedicado que se encargue de procesar los mensajes del topic de errores 
+                     * o enviar notificaciones sobre los errores.
+                     */
                 }
             }
         })
